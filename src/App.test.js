362e9b2edd4b3de2fe8f442c6c1ea7b7e59a8b60
Yasser <yassergeo@gmail.com>
2020-09-28 16:11:55 -0500

    fixed test

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+import React from "react";
+//import ReactDOM from "react-dom";
+import App from "./App";
+import { render } from "@testing-library/react";
+import { CheckBoxProvider } from "./components/Context/CheckBoxContext";
+beforeEach(() => {
+  render(
+    <CheckBoxProvider>
+      <App></App>
+    </CheckBoxProvider>
+  );
+});
+it("renders without crashing", () => {
+  //const div = document.createElement("div");
+  //ReactDOM.render(<App />, div);
+  //ReactDOM.unmountComponentAtNode(div);
 });
