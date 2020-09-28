ce3b5bdba2e4609ac9dd56ab4ff274816950e4ff
Yasser <yassergeo@gmail.com>
2020-09-28 11:44:40 -0500

    added open bank logo and key Realizando el componente de la primera pagina del wizar #3

--- src/components/WizardPages/index.js
+++ src/components/WizardPages/index.js
@@ -1,7 +1,9 @@
 // eslint-disable-next-line
 import React from "react";
 import Welcome from "./Welcome";
-//import { Container, Row } from "reactstrap";
+import logo_openbank from "../../assets/img/logo_openbank.png";
+import key_openbank from "../../assets/img/key_openbank.png";
+import { Row, Col } from "reactstrap";
 /** @jsx jsx */
 import { jsx } from "@emotion/core";
 
@@ -12,11 +14,29 @@ const titleStyle = {
   fontSize: 25,
   fontWeight: "bold",
 };
+const keyOpenbakStyles = {
+  height: 45,
+  width: 55,
+};
 
 const MainWizardPages = () => {
   return (
     <div>
-      <div css={titleStyle}>Crea tu password manager</div>
+      <Row>
+        <Col>
+          <div css={titleStyle}>Crea tu password manager</div>
+        </Col>
+        <Col>
+          <Row className="justify-content-end">
+            <img src={logo_openbank} alt={"openbank-logo"} />
+            <img
+              src={key_openbank}
+              alt={"openbank-key"}
+              css={keyOpenbakStyles}
+            />
+          </Row>
+        </Col>
+      </Row>
       <Welcome />
     </div>
   );
