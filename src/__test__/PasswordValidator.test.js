12adbcd77b6ea9ac191bf74b97e598c1022f3f40
Yasser <yassergeo@gmail.com>
2020-09-29 10:40:19 -0500

    added max length test Agregar la logica del formulario con las validaciones #16

--- src/__test__/PasswordValidator.test.js
+++ src/__test__/PasswordValidator.test.js
@@ -5,4 +5,6 @@ it("Should input a password between 8 to 24 characters which contain at least on
   expect(passwordValidator(Correct_password)).toBe(true);
   const Incorrect_password = "8DSDF";
   expect(passwordValidator(Incorrect_password)).toBe(false);
+  const Incorrect_password_max_length = "8DSDFasdeddsdasdasdasdasdadad";
+  expect(passwordValidator(Incorrect_password_max_length)).toBe(false);
 });
